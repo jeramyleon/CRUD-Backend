3834503ff463f7b35d47735029783bb624b30fdb
BTBurton1 <bilaldasher9@gmail.com>
2025-07-02 20:21:02 -0400

    Added the full student model for sure this time

---
+++ models/student.js
@@ -0,0 +1,62 @@
+const {Sequelize, DataTypes}= require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const students = sequelize.define(
+    'students',
+    {
+        firstName:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        lastName:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        email:{
+            type: DataTypes.TEXT,
+            allowNull: false,
+             validate:{
+                notEmpty: true,
+                isEmail: true,
+            },
+        },
+        imageUrl:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'https://cdn-icons-png.flaticon.com/512/70/70906.png',
+        },
+        gpa:{
+            type:DataTypes.DECIMAL(3, 2),
+            allowNull: false,
+            validate:{
+                min:0.0,
+                max:4.0,
+            },
+        },
+        description:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notEmpty: true,
+            },
+        },
+    }
+);
+
+module.exports = students;
+
+students.sync().then(() => {
+  console.log("Students table works!");
+});
+sequelize.sync({ force: true }).then(() => {
+  console.log("Sequelize table operational");
+});
+students.belongsTo(campus);
+campus.hasMany(students);
+
