21b7c5b42d2ce8bbd339d8c1e1fd44ea70cf934c
BTBurton1 <bilaldasher9@gmail.com>
2025-07-04 19:17:05 -0400

    Finished up the campus routes planning to make a put http method if necessary later

--- api/campuses.js
+++ api/campuses.js
@@ -1,10 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const {Campus} = require('../models');
-
-router.get("/",(req, res)=>{
-    res.send("All campuses")
-})
+//Gets every single campus
+router.get("/", async(req, res)=>{
+    try{
+        const campus = await Campus.findAll();
+        res.json(campus);
+    }
+    catch(err){
+        res.status(500).json({error:'Failed to fetch campuses'})
+    }
+});
+router.post("/", async(req ,res)=>{
+    try{
+        const campus = await Campus.create(req.body);
+        res.status(201).json(campus);
+    }
+    catch(err){
+        res.status(400).json({error:err.message});
+    }
+});
+//gets one campus specifically by ID
+router.get("/:id", async (req,res)=>{
+    try{
+        const campus = await Campus.findByPk(req.params.id);
+        if(campus){
+            res.json(campus);
+        }
+        else{
+            res.status(404).json({error:"Campus not found"});
+        }
+    }
+    catch(error){
+        res.status(500).json({error:"Failed to fetch the specified campus data"});
+    }
+});
+//deletes campuses by specified id
+router.delete("/:id", async (req, res)=>{
+    try{
+        const erased = await Campus.destroy({
+            where: { id: req.params.id},
+        });
+         if(erased){
+            res.status(204).send();
+        }
+        else{
+            res.status(404).json({error:"Campus not found"});
+        }
+    }
+     catch(err){
+            res.status(500).json({error:"Failed to delete campus"});
+        }      
+    });
 
 
 module.exports = router;
